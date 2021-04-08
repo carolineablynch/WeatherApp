f94b0a23e5081594b21bb6878584b25a9d559fac
Caroline Lynch <carolineablynch@gmail.com>
2021-04-08 19:53:39 -0400

    getting cities

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
 require("isomorphic-fetch");
 const fs = require("fs");
-const router = require("express").Router();
+const router = require("express").Router();router.
+
+get("/api/autocomplete/:city", async (req, res) => {
+    const { city } = req.params;
+    const cities = JSON.parse(fs.readFileSync("./cities.json")).filter(cityData =>
+      cityData.city.toLowerCase().startsWith(city.toLowerCase())
+    );
+  
+    res.json(cities);
+  });

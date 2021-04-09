8a72eafd6ae911b0a3914ee67a62a7d2fb903ef0
Caroline Lynch <carolineablynch@gmail.com>
2021-04-08 20:38:25 -0400

    search function

--- src/Search.js
+++ src/Search.js
@@ -3,3 +3,18 @@ import { Form, ListGroup, Accordion, Card, Button } from "react-bootstrap";
 import Downshift from "downshift";
 import debounce from "debounce";
 import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
+
+export default function Search({ setLocation }) {
+    const [autoCompleteList, setAutoCompleteList] = useState(null);
+    const [cityInput, setCityInput] = useState(null);
+    const [open, setOpen] = useState(false);
+    const [history, setHistory] = useState(JSON.parse(localStorage.getItem("history")));
+    const fetchAutoComplete = value => {
+      if (value) {
+        fetch(`/api/autocomplete/${value}`)
+          .then(response => response.json())
+          .then(cityData => {
+            if (cityData) setAutoCompleteList(cityData);
+          });
+      }
+    };

e39939cd7048e7ac8715341d6e01b58076c70fd4
Caroline Lynch <carolineablynch@gmail.com>
2021-04-08 20:17:12 -0400

    App.js building

--- src/App.js
+++ src/App.js
@@ -3,3 +3,26 @@ import { Container } from "react-bootstrap";
 import Loader from "react-loader-spinner";
 import Search from "./Search";
 import Weather from "./Weather";
+
+function App() {
+    const [location, setLocation] = useState(JSON.parse(localStorage.getItem("location")));
+    const [weatherData, setWeatherData] = useState(null);
+    const [fetching, setFetching] = useState(false);
+    const [error, setError] = useState(null);
+  
+    useEffect(async () => {
+        if (location)
+          try {
+            setFetching(true);
+    
+            const { lat, lng } = location;
+            const response = await fetch(`/api/weather/${lat}/${lng}`);
+    
+            setFetching(false);
+            setWeatherData(await response.json());
+            setError(null);
+          } catch {
+            setFetching(false);
+            setError("Unable to retrieve weather data");
+          }
+      }, [location]);

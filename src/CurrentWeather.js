3e771951891c86f38e18a1c6df4702320de7b20a
Caroline Lynch <carolineablynch@gmail.com>
2021-04-08 20:18:42 -0400

    uvindex color function

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,2 +1,9 @@
 import { Card } from "react-bootstrap";
 import { FaThermometerHalf, FaWind } from "react-icons/fa";
+
+function uvIndexColor(uvIndex) {
+    if (uvIndex <= 3) return "rgba(0, 120, 6, 0.6)";
+    if (uvIndex <= 6) return "rgba(208, 119, 0, 0.6)";
+  
+    return "rgba(208, 0, 0, 0.6)";
+  }

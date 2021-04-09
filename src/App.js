fbe292a3d1157708d8594d359630440d620bc379
Caroline Lynch <carolineablynch@gmail.com>
2021-04-08 20:16:19 -0400

    creating App.js file, importing requirements

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import Loader from "react-loader-spinner";
+import Search from "./Search";
+import Weather from "./Weather";
